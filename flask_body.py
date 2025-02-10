8ef2dfbac6cf9dc9b040595f42a1a1f02111faca
Shubham Mundhra <shubham23official@gmail.com>
2025-02-11 01:11:44 +0530

    made fixes to html file and flask file

--- flask_body.py
+++ flask_body.py
@@ -2,9 +2,9 @@ from flask import Flask,render_template,request
 import pyshorteners
 class Flask_body:
     app=Flask(__name__)
-    @app.route("/",methods=['POST','GET'])
+    @app.route("/",methods=["POST","GET"])
     def home():
-        if request.method=='POST':
+        if request.method=="POST":
             url_rec=request.form["url"]
             short_url=pyshorteners.Shortener().tinyurl.short(url_rec)
             return render_template("form.html",new_url=short_url,old_url=url_rec)
