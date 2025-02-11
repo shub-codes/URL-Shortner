86ae65386acebd26b3135c0a54a80ac2f74e925b
Shubham Mundhra <shubham23official@gmail.com>
2025-02-12 01:21:17 +0530

    adding QR code functionality

--- flask_body.py
+++ flask_body.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,request
+from qrcode import QRCode
+# QR code generation depends on pillow library and QRcode library
 import pyshorteners
 class Flask_body:
     app=Flask(__name__)
@@ -7,7 +9,13 @@ class Flask_body:
         if request.method=="POST":
             url_rec=request.form["url"]
             short_url=pyshorteners.Shortener().tinyurl.short(url_rec)
-            return render_template("form.html",new_url=short_url,old_url=url_rec)
+            qr = QRCode(version=1, box_size="3", border="1")
+            qr.add_data(url_rec)
+            qr.make(fit=True)
+            img = qr.make_image(fill_color="black", back_color="white")
+            qr_path="static/QRcode.png"
+            img.save(qr_path)
+            return render_template("form.html",new_url=short_url,old_url=url_rec,qr_path=qr_path)
         else:
             return render_template("form.html")
 
